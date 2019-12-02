ca77ed3ec21f80d328080fa1e861454614f23e2a
Lucas Land <lucas@marke.com.br>
2019-12-01 21:39:54 -0300

    Seleção de conversão com data dos últimos 30 dias

--- app/src/main/java/com/example/conversor/ExchangeRater.java
+++ app/src/main/java/com/example/conversor/ExchangeRater.java
@@ -3,11 +3,14 @@ package com.example.conversor;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -26,32 +29,65 @@ public class ExchangeRater {
 
     public ExchangeRater(Context context) {
         db = DatabaseHelper.getInstance(context);
-        this.labels = new ArrayList<String>();
+        this.labels = new ArrayList<>();
         this.ratesDate = new Date(System.currentTimeMillis());
-        this.rates = db.getRatesMapbyDate(this.ratesDate);
-        if ( ! this.rates.isEmpty() ) {
-            Iterator it = this.rates.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry)it.next();
-                this.labels.add(pair.getKey().toString());
+        this.rates = new HashMap<>();
+    }
+
+    public int loadMonthlyRates() {
+        boolean fetchedJSON = false;
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(new Date());
+        startCalendar.add(Calendar.DAY_OF_YEAR, -30);
+        Calendar endCalendar = Calendar.getInstance();
+        endCalendar.setTime(new Date());
+        endCalendar.get(Calendar.DAY_OF_YEAR);
+        while (startCalendar.before(endCalendar) || startCalendar.equals(endCalendar)) {
+            Date date = startCalendar.getTime();
+            if(db.getRatesMapbyDate(date).isEmpty()) {
+                try {
+                    this.fetchRatesByDate(date);
+                    fetchedJSON = true;
+                } catch (JSONException | ExecutionException | InterruptedException e) {
+                    return 0;
+                }
             }
-            Collections.sort(this.labels, String.CASE_INSENSITIVE_ORDER);
+            startCalendar.add(Calendar.DATE, 1);
+        }
+        if (fetchedJSON) {
+            return 2;
+        } else {
+            return 1;
         }
     }
 
+    public void changeRatesByDate(Date date) {
+        Log.v("changeRatesByDate()", "alterando para cotações de " + new SimpleDateFormat("dd/MM/yyyy").format(date));
+        this.ratesDate = date;
+        this.rates = db.getRatesMapbyDate(date);
+        this.labels.clear();
+        Iterator it = this.rates.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            this.labels.add(pair.getKey().toString());
+        }
+        Collections.sort(this.labels, String.CASE_INSENSITIVE_ORDER);
+    }
+
     @SuppressLint("StaticFieldLeak")
-    public void fetchNewRates() throws JSONException, ExecutionException, InterruptedException {
-        String out = new JsonDownloader().execute("https://api.exchangeratesapi.io/latest?base=BRL").get();
+    public void fetchRatesByDate(Date date) throws JSONException, ExecutionException, InterruptedException {
+        String out = new JsonDownloader().execute("https://api.exchangeratesapi.io/" + new SimpleDateFormat("yyyy-MM-dd").format(date) + "?base=BRL").get();
         exchangeRatesJSON = new JSONObject(out);
         JSONObject jsonRates = exchangeRatesJSON.getJSONObject("rates");
-        this.rates = new HashMap<String,Double>();
+        this.rates = new HashMap<>();
+        this.labels.clear();
         Iterator<String> iter = jsonRates.keys();
         while (iter.hasNext()) {
             String key = iter.next();
             this.labels.add(key);
             this.rates.put(key,Double.parseDouble(jsonRates.getString(key)));
         }
-        this.ratesDate = new Date(System.currentTimeMillis());
+        this.ratesDate = date;
         db.addRates(this);
     }
 
--- app/src/main/java/com/example/conversor/MainActivity.java
+++ app/src/main/java/com/example/conversor/MainActivity.java
@@ -4,47 +4,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class MainActivity extends AppCompatActivity {
 
     ExchangeRater exchangeRater;
+    Spinner dateSelector;
     Spinner fromCurrency;
     EditText fromValue;
     Spinner toCurrency;
     EditText toValue;
     Button convert;
+    Toast toast;
+    ArrayList<String> datesInRange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        exchangeRater = new ExchangeRater(getBaseContext());
+        datesInRange = new ArrayList<>();
+
+        dateSelector = findViewById(R.id.dateSelector);
         fromCurrency = findViewById(R.id.fromCurrency);
         fromValue = findViewById(R.id.fromValue);
         toCurrency = findViewById(R.id.toCurrency);
         toValue = findViewById(R.id.toValue);
         convert = findViewById(R.id.convert);
 
-        exchangeRater = new ExchangeRater(getBaseContext());
-        if ( exchangeRater.getRates().isEmpty() ) {
-            Toast toast;
-            try {
-                exchangeRater.fetchNewRates();
-                toast = Toast.makeText(getBaseContext(), "Cotações atualizadas!", Toast.LENGTH_LONG);
-            } catch (Exception e) {
-                toast = Toast.makeText(getBaseContext(), "Não foi possível atualizar as cotações!", Toast.LENGTH_LONG);
-                e.printStackTrace();
-            }
-            toast.setGravity(Gravity.CENTER, 0, 0);
-            toast.show();
+        switch (exchangeRater.loadMonthlyRates()) {
+            case 0:
+                toast = Toast.makeText(getBaseContext(), "Não foi possível carregar novas cotações!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                break;
+            case 1:
+                exchangeRater.changeRatesByDate(new Date());
+                populateDateSelector();
+                break;
+            case 2:
+                exchangeRater.changeRatesByDate(new Date());
+                populateDateSelector();
+                toast = Toast.makeText(getBaseContext(), "Cotações atualizadas online!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                break;
         }
+
+        ArrayAdapter<String> dateSelectorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, datesInRange);
+        dateSelectorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateSelector.setAdapter(dateSelectorAdapter);
+        dateSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                try {
+                    Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dateSelector.getSelectedItem().toString());
+                    exchangeRater.changeRatesByDate(date);
+                    if (!toValue.getText().toString().isEmpty()) {
+                        showConversionResult();
+                    }
+                } catch (ParseException e) {
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("ITEM", "NOT");
+            }
+        });
 
         ArrayAdapter<String> fromCurrencyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, exchangeRater.getLabels());
         fromCurrencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -57,8 +99,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnConvertClick(View v) {
-        Double fromNumber = (Double.parseDouble((fromValue.getText().toString())));
-        if (fromNumber > 0) {
+        showConversionResult();
+    }
+
+    private void showConversionResult() {
+        if (!fromValue.getText().toString().isEmpty()) {
+            Double fromNumber = (Double.parseDouble((fromValue.getText().toString())));
             toValue.setText(exchangeRater.convertCurrency(fromCurrency.getSelectedItem().toString(), fromNumber, toCurrency.getSelectedItem().toString()).toString());
         }
     }
@@ -68,5 +114,17 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void populateDateSelector() {
+        datesInRange = new ArrayList<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DAY_OF_YEAR, -30);
+        Calendar endCalendar = new GregorianCalendar();
+        endCalendar.setTime(new Date());
+        while (calendar.before(endCalendar) || calendar.equals(endCalendar)) {
+            datesInRange.add(new SimpleDateFormat("dd/MM/yyyy").format(endCalendar.getTime()));
+            endCalendar.add(Calendar.DATE, -1);
+        }
+    }
 
 }
